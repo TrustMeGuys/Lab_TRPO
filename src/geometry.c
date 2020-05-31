33b1600bf55f67dff04aa2c2b6e73d60e6ca29d4
Egor Bozhko <vanya.sosister@mail.ru>
2020-06-01 00:10:32 +0700

    Add function "calculating_circles" in file "geometry.c",which counts the perimeter and area.

--- src/geometry.c
+++ src/geometry.c
@@ -5,6 +5,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+int calculating_circles(int z, float* circles_radius, float* all_circles)
+{
+    float pi = 3.1415;
+    int g = 0, t = 1;
+    for (int i = 0; i < z; i++) {
+        float perimetr = circles_radius[i] * pi * 2,
+              area = circles_radius[i] * circles_radius[i] * pi;
+        g = i;
+        printf("Окружность №%d (%-7.3f,%-7.3f)\n",
+               t,
+               all_circles[g],
+               all_circles[g + 1]);
+        printf("P = %-10.4f\n", perimetr);
+        printf("S = %-10.4f\n", area);
+        g = g + 2;
+        t++;
+    }
+    return 0;
+}
+
 int intersection_circles(
         int number_circles, float* all_circles, float* circles_radius)
 {
